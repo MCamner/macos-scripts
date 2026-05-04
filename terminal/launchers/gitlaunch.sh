4f93f4f585ccd94f25332c010a24ba3ff810bd37
McAmner <mattias.camner@gmail.com>
2026-05-04 03:33:13 +0200

    update shell scripts

--- terminal/launchers/gitlaunch.sh
+++ terminal/launchers/gitlaunch.sh
@@ -376,6 +376,7 @@ function prompt_choice() {
       --cursor.foreground="229" \
       --item.foreground="229" \
       --selected.foreground="220" \
+      --help.foreground="255" \
       --height=9 \
       "1. GIT STATUS" \
       "2. GIT PULL" \
